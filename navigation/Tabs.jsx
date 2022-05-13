e7e609588e241592bc024801b0ec741caa9bdfb9
Ian Park <ian.park@sodacrew.com>
2022-05-13 17:15:13 +0900

    Add: add TabBar Icon

--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
+import { Ionicons } from "@expo/vector-icons";
 import { theme } from "../colors";
 
 const Tab = createBottomTabNavigator();
@@ -25,11 +26,40 @@ const Tabs = () => {
         headerTitleStyle: {
           color: palette.text,
         },
+        tabBarLabelStyle: {
+          paddingTop: -5,
+          fontSize: 12,
+          fontWeight: "600",
+        },
       }}
     >
-      <Tab.Screen name="Movies" component={Movie}></Tab.Screen>
-      <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
-      <Tab.Screen name="Search" component={Search}></Tab.Screen>
+      <Tab.Screen
+        name="Movies"
+        component={Movie}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film-outline" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
     </Tab.Navigator>
   );
 };
