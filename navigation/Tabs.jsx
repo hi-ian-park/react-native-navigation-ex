ff3da8d02753a2b9f3b75dd5f930bd2b21da0dda
Ian Park <ian.park@sodacrew.com>
2022-05-13 12:44:23 +0900

    try changing theme dark mode / light mode

--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -1,17 +1,37 @@
 import React from "react";
+import { useColorScheme } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Search from "../screens/Search";
 import Tv from "../screens/Tv";
+import { theme } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movie}></Tab.Screen>
-    <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
-    <Tab.Screen name="Search" component={Search}></Tab.Screen>
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const colorScheme = useColorScheme();
+  const palette = theme[colorScheme];
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: palette.background,
+        },
+        tabBarActiveTintColor: palette.tint,
+        tabBarInactiveTintColor: palette.inactive,
+        headerStyle: {
+          backgroundColor: palette.background,
+        },
+        headerTitleStyle: {
+          color: palette.text,
+        },
+      }}
+    >
+      <Tab.Screen name="Movies" component={Movie}></Tab.Screen>
+      <Tab.Screen name="Tv" component={Tv}></Tab.Screen>
+      <Tab.Screen name="Search" component={Search}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
