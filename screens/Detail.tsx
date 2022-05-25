71bb77524d179c3f9b04157a2302b6a5c5ddc43a
Ian Park <ian.park@sodacrew.com>
2022-05-25 16:26:48 +0900

    Add: Implement Detail Header

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,17 +1,51 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, StyleSheet, useColorScheme } from 'react-native';
 import styled from 'styled-components/native';
 
-const Detail = ({ navigation, route }) => {
+import { Movie, TV } from '../api';
+import { theme } from '../colors';
+import Poster from '../components/Poster';
+import { makeImgPath } from '../utils';
+
+type RootStackParamList = {
+  Detail: Movie | TV;
+};
+
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, 'Detail'>;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const Detail: React.FC<DetailScreenProps> = ({ navigation, route }) => {
+  const colorScheme = useColorScheme() || 'light';
   const { setOptions } = navigation;
-  const { originalTitle } = route.params;
+  const { params } = route;
 
   useEffect(() => {
-    setOptions({ title: originalTitle });
+    const isMovie = 'original_title' in params;
+    setOptions({ title: isMovie ? 'Movie' : 'TV Show' });
   }, []);
+
   return (
     <Styled.Container>
-      <Text>{originalTitle}</Text>
+      <Styled.Header>
+        <Styled.Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || '') }}
+        />
+        <LinearGradient
+          colors={['transparent', theme[colorScheme].background]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Styled.Column>
+          <Poster path={params.poster_path || ''} />
+          <Styled.Title>
+            {params.original_title || params.original_name}
+          </Styled.Title>
+        </Styled.Column>
+      </Styled.Header>
+      <Styled.Overview>{params.overview}</Styled.Overview>
     </Styled.Container>
   );
 };
@@ -21,5 +55,33 @@ export default Detail;
 const Styled = {
   Container: styled.ScrollView`
     background-color: ${({ theme }) => theme.background};
+  `,
+
+  Header: styled.View`
+    height: ${SCREEN_HEIGHT / 4};
+    justify-content: flex-end;
+    padding: 0px 10px;
+  `,
+
+  Background: styled.Image``,
+
+  Column: styled.View`
+    flex-direction: row;
+    width: 80%;
+    margin-bottom: 30px;
+    padding: 0 20px;
+  `,
+
+  Title: styled.Text`
+    color: ${({ theme }) => theme.text};
+    font-size: 32px;
+    align-self: flex-end;
+    margin-left: 15px;
+    font-weight: 600;
+  `,
+
+  Overview: styled.Text`
+    color: ${({ theme }) => theme.text};
+    padding: 0 20px;
   `,
 };
